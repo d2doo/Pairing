596d0968d1ae3bf54c85fdb36492f61aac5af012
amivayun <wntjrdbs@gmail.com>
2024-03-11 15:44:47 +0900

    refactor: project

--- src/main/java/com/ssafy/i10a709be/common/exception/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.i10a709be.common.exception;
-import org.springframework.http.HttpStatus;
-
-
-public interface ErrorCode {
-    HttpStatus getHttpStatus();
-    String getErrorName();
-    String getErrorMessage();
-}
