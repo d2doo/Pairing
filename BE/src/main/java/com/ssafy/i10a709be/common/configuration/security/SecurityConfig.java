863ab651e9d65fdb36e670ce5c89d68cf5546fe8
Hunnibs <cqqudgjs@hufs.ac.kr>
2024-03-12 17:27:44 +0900

    fix: annotation 추가 및 권한 설정 해제

--- BE/src/main/java/com/ssafy/i10a709be/common/configuration/security/SecurityConfig.java
+++ BE/src/main/java/com/ssafy/i10a709be/common/configuration/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.ssafy.i10a709be.common.configuration.security;
 
 import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,6 +15,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import static jakarta.servlet.DispatcherType.ERROR;
 import static jakarta.servlet.DispatcherType.FORWARD;
 
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,6 +26,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .dispatcherTypeMatchers(FORWARD, ERROR).permitAll()
+                        .requestMatchers("**")
+                        .permitAll()
                         .anyRequest().authenticated()
                 );
 
