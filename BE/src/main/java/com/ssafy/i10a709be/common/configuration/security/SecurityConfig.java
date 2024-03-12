2b1634cedf1c7503b44a853ad2b820acedfe114d
amivayun <wntjrdbs@gmail.com>
2024-03-12 17:34:58 +0900

    feat: 이미지 업로드 기능

--- BE/src/main/java/com/ssafy/i10a709be/common/configuration/security/SecurityConfig.java
+++ BE/src/main/java/com/ssafy/i10a709be/common/configuration/security/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.ssafy.i10a709be.common.configuration.security;
 
 import java.util.Arrays;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,6 +16,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import static jakarta.servlet.DispatcherType.ERROR;
 import static jakarta.servlet.DispatcherType.FORWARD;
 
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,6 +27,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .dispatcherTypeMatchers(FORWARD, ERROR).permitAll()
+                        .requestMatchers("**")
+                        .permitAll()
                         .anyRequest().authenticated()
                 );
 
@@ -29,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://i10a709.p.ssafy.io"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://i10a709.p.ssafy.io","https://ssafyhelper.shop"));
         config.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE", "PATCH"));
         config.setAllowCredentials(true);
         config.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "x-requested-with"));
--- BE/src/main/java/com/ssafy/i10a709be/common/exception/CustomErrorCode.java
+++ BE/src/main/java/com/ssafy/i10a709be/common/exception/CustomErrorCode.java
@@ -12,7 +12,7 @@ public enum CustomErrorCode implements ErrorCode{
     INTERNAL_SERVER_ERROR(
             HttpStatus.INTERNAL_SERVER_ERROR,
             "잠시 후 다시 시도해주세요. 문제가 반복될 시에 wntjrdbs@gmail.com에 연락해주세요."
-            );
+            ), FILE_IS_NOT_IMAGE_TYPE(HttpStatus.INTERNAL_SERVER_ERROR,"이미지 파일 형식이 지원하지 않습니다.") ;
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
