dff56f42914927aa9080c0b582b7f238811485aa
amiva <wntjrdbs@gmail.com>
2024-03-15 04:01:01 +0900

    init: repository

---
+++ FE/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "@/components/Header.tsx";
+import MainLogo from "@/components/MainLogo";
+function App() {
+  return (
+    <div className="bg-gradient-to-b from-blue1 to-white1">
+      <Header />
+      <MainLogo />
+    </div>
+  );
+}
+
+export default App;
