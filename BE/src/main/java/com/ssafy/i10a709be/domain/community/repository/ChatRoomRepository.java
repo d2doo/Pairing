73869ea65d5f33240fccf4d9c94216b729f568b1
kjy0349 <kjy0349@naver.com>
2024-03-18 17:00:36 +0900

    chore: 테스트용 Query 추가

--- BE/src/main/java/com/ssafy/i10a709be/domain/community/repository/ChatRoomRepository.java
+++ BE/src/main/java/com/ssafy/i10a709be/domain/community/repository/ChatRoomRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.i10a709be.domain.community.repository;
 import com.ssafy.i10a709be.domain.community.entity.ChatRoom;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     @Query("select crooms from ChatRoom crooms where crooms.member.memberId =: memberId")
