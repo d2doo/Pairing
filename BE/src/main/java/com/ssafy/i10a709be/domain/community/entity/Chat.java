aa064cb680da6a95d0bf6b0b5fc3db2de2ae37f1
kjy0349 <kjy0349@naver.com>
2024-03-18 14:56:02 +0900

    feat : 채팅 관련 Entity, Enum 추가 #15

---
+++ BE/src/main/java/com/ssafy/i10a709be/domain/community/entity/Chat.java
@@ -0,0 +1,39 @@
+package com.ssafy.i10a709be.domain.community.entity;
+
+import com.ssafy.i10a709be.domain.community.enums.ChatType;
+import com.ssafy.i10a709be.domain.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter @Builder
+@RequiredArgsConstructor
+public class Chat {
+    @Id @GeneratedValue
+    private long chatId;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne
+    private Member member;
+
+    @JoinColumn(name = "chat_room_id")
+    @ManyToOne
+    private ChatRoom chatRoom;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private ChatType type;
+
+    private long fileId;
+}
---
+++ BE/src/main/java/com/ssafy/i10a709be/domain/community/entity/ChatRoom.java
@@ -0,0 +1,43 @@
+package com.ssafy.i10a709be.domain.community.entity;
+
+import com.ssafy.i10a709be.domain.community.enums.ChatRoomStatus;
+import com.ssafy.i10a709be.domain.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter @Builder
+@RequiredArgsConstructor
+public class ChatRoom {
+    @Id @GeneratedValue
+    private long chatRoomId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String title;
+
+    private int capability;
+
+    @Enumerated(EnumType.STRING)
+    private ChatRoomStatus status;
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<Chat> chats = new ArrayList<>();
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<UserChatRoom> userChatRooms = new ArrayList<>();
+}
