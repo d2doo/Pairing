5facc82eeea789cacddf01485acee81d348ca9ac
kjy0349 <kjy0349@naver.com>
2024-03-18 14:56:02 +0900

    feat : 채팅 관련 Entity, Enum 추가 #15

---
+++ BE/src/main/java/com/ssafy/i10a709be/domain/community/enums/ChatRoomStatus.java
@@ -0,0 +1,5 @@
+package com.ssafy.i10a709be.domain.community.enums;
+
+public enum ChatRoomStatus {
+    활성화, 비활성화
+}
---
+++ BE/src/main/java/com/ssafy/i10a709be/domain/community/enums/ChatType.java
@@ -0,0 +1,5 @@
+package com.ssafy.i10a709be.domain.community.enums;
+
+public enum ChatType {
+    메세지, 이미지
+}
