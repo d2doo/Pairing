fea04aa2e2c07ed5cf8fa98f02571e2b026658c7
정지수 <hzxvl@naver.com>
2024-03-25 09:55:25 +0900

    [FE#32] 조립 상품 등록 레이아웃
    * design: 조립등록 레이아웃 #32
    
    카테고리 UI는 추후 넣을 예정
    
    * design: 뒤로가기, badge #32
    
    뒤로가기 설정, badge custom 후 ProductTypeC.tsx 파일에 적용

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -4,10 +4,14 @@ import LoginPage from "@/pages/LoginPage";
 import MainPage from "@/pages/MainPage";
 import ChatPage from "@/pages/ChatPage";
 import ChatRoom from "@/pages/ChatRoomPage";
-import ChatList from "./components/ChatList";
-import SalePage from "./page/SalePage";
-import SaleMain from "./components/SaleMain";
+import ChatList from "@/components/ChatList";
+import SalePage from "@/pages/SalePage";
+import SaleMain from "@/components/SaleMain";
 import {KakaoAuthCallback} from "@/pages/auth/KakaoAuthCallback.tsx";
+import SaleHeader from "@/components/SaleHeader";
+import UnitLists from "@/components/SaleUnitLists";
+import FindUnit from "@/components/SaleFindUnit";
+
 function App() {
   return (
     <>
@@ -18,6 +22,11 @@ function App() {
             <Route path="/new" element={<SaleMain />} />
             {/* 개별 상품 등록 route */}
             {/* <Route path="/new/unit" element={<컴포넌트 넣어라. />} /> */}
+            <Route element={<SaleMain />} />
+            <Route element={<SaleHeader />}>
+              <Route path="/new/unit-lists" element={<UnitLists />} />
+              <Route path="/new/product" element={<FindUnit />} />
+            </Route>
           </Route>
           <Route element={<ChatPage />}>
             <Route path="/chat" element={<ChatList />} />
--- FE/src/components/ProductTypeC.tsx
+++ FE/src/components/ProductTypeC.tsx
@@ -1,20 +1,18 @@
+import { Badge } from "@/components/ui/badge";
+
 function ProductTypeC() {
   return (
     <>
       <div className="mx-8 box-border flex justify-between border-2 border-solid border-black1 p-4">
         <img
-          src="img/extra-product-img.png"
+          src="/img/extra-product-img.png"
           alt="product_err"
           className="size-20 object-cover"
         />
         <div className="flex h-20 w-44 flex-col justify-center px-2 font-GothicLight">
-          <div className="flex text-[0.563rem]">
-            <div className="w-[3.375rem] rounded-xl border border-blue1 text-center">
-              왼쪽
-            </div>
-            <div className="w-[3.375rem] rounded-xl border border-blue1 text-center">
-              라이브
-            </div>
+          <div className="flex text-xxs">
+            <Badge>왼쪽</Badge>
+            <Badge>라이브</Badge>
           </div>
           <div className="my-1 text-xs">버즈 라이브 왼쪽(조합ㄴㄴ)</div>
           <div className="text-xs">₩ 30,000</div>
