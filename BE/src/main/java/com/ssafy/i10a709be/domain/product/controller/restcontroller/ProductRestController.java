c8f5a1e2efe5ec9bd311403187b3782b117a8438
이병헌 <cqqudgjs@hufs.ac.kr>
2024-03-29 10:37:46 +0900

    [BE #60] 상품 등록, 조회 수정
    * fix: 상품 전체 조회 수정 #60
    
    * feat: 상품 개별 등록 완료 #60
    
    * chore: toString 삭제 #60

--- BE/src/main/java/com/ssafy/i10a709be/domain/product/controller/restcontroller/ProductRestController.java
+++ BE/src/main/java/com/ssafy/i10a709be/domain/product/controller/restcontroller/ProductRestController.java
@@ -43,6 +43,7 @@ public class ProductRestController {
         log.info( productSaveRequestDto.toString());
         log.info( productSaveRequestDto.getUnit().toString());
         try {
+            log.info(memberId);
             Product product = productService.saveProduct(memberId, productSaveRequestDto);
             log.info("products!!:" + product.toString() );
         } catch (IllegalArgumentException e) {
@@ -55,12 +56,12 @@ public class ProductRestController {
 
     @GetMapping
     public ResponseEntity<List<ProductFindResponseDto>> findAllProduct(
-            @AuthenticationPrincipal String memberId,
             @RequestParam int page,
             @RequestParam int size,
             @RequestParam(required = false) Long productId,
             @RequestParam(required = false) Boolean isCombined,
             @RequestParam(required = false) String nickname,
+            @RequestParam(required = false) String memberId,
             @RequestParam(required = false) Long categoryId,
             @RequestParam(required = false) String productStatus,
             @RequestParam(required = false) Integer startPrice,
