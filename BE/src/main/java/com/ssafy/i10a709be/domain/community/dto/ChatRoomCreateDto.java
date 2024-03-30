69d89a711afdda1fb44802c691925094646f44cf
이병헌 <cqqudgjs@hufs.ac.kr>
2024-03-30 15:44:36 +0900

    [BE #63] 거래 시작, 완료 기능 구현
    * fix: 삭제된 상품 조회 버그 수정 #63
    
    * feat: 거래 시작, 완료 기능 구현 #60
    
    거래 시작 시 채팅 방 생성까지 기능 구현 완료

--- BE/src/main/java/com/ssafy/i10a709be/domain/community/dto/ChatRoomCreateDto.java
+++ BE/src/main/java/com/ssafy/i10a709be/domain/community/dto/ChatRoomCreateDto.java
@@ -4,9 +4,11 @@ import com.ssafy.i10a709be.domain.community.enums.ChatRoomStatus;
 import com.ssafy.i10a709be.domain.member.entity.Member;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
+@Builder
 @Getter
 @AllArgsConstructor
 public class ChatRoomCreateDto {
